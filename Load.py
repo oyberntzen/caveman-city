00e7aa4aec62de511079f6fc44d7e32abb2b900d
BuildTools <oyberntzen@gmail.com>
2017-10-24 18:48:10 +0200

    Deleted useless file.

--- Load.py
+++
@@ -1,9 +0,0 @@
-import Stat
-
-
-
-def load():
-    for Monster in Stat.MonsterImp:
-        monster = Monster.Main()
-        Stat.MonsterGroup.add(monster)
-        Stat.MonserList.append(monster)
